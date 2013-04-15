f5fcb53a006fde03b88f84d8e4e5526fdfbfee43
Christian Lønaas <christian.lonaas@discombobulator.org>
2013-04-15 21:14:22 +0200

    Added some find-methods.

--- src/temperature.rb
+++ src/temperature.rb
@@ -3,6 +3,7 @@ require 'open-uri'
 require 'chronic'
 require 'yaml'
 
+$LOAD_PATH.unshift File.dirname(__FILE__)
 require 'util/logging'
 require 'util/persistence'
 
@@ -33,6 +34,68 @@ class Temperature
     })
   end
 
+  def self.find_all
+    temperature_rows = self.find_all
+    if nil == temperature_rows
+      return []
+    end
+    temperatures = []
+    temperature_rows.each do |temperature_reading, source, timestamp|
+      temperatures << Temperature.new(temperature_reading, source, timestamp)
+    end
+    temperatures
+  end
+
+  def self.find_by_source(source)
+    temperature_rows = self.find_by_column(:source, source)
+    if nil == temperature_rows
+      return []
+    end
+    temperatures = []
+    temperature_rows.each do |temperature_reading, source, timestamp|
+      temperatures << Temperature.new(temperature_reading, source, timestamp)
+    end
+    temperatures
+  end
+
+  def self.find_highest(limit)
+    sql = "SELECT * from #{self.inspect} ORDER BY temperature_reading DESC LIMIT #{limit}"
+    logger.debug sql
+    begin
+      stm = db.prepare sql
+      rs = stm.execute
+    rescue SQLite3::SQLException => e
+      puts e.backtrace
+      rs = nil
+    end
+    if rs != nil
+      temperatures = []
+      rs.each do |temperature_reading, source, timestamp|
+        temperatures << Temperature.new(temperature_reading, source, timestamp)
+      end
+      temperatures
+    end
+  end
+
+  def self.find_lowest(limit)
+    sql = "SELECT * from #{self.inspect} ORDER BY temperature_reading ASC LIMIT #{limit}"
+    logger.debug sql
+    begin
+      stm = db.prepare sql
+      rs = stm.execute
+    rescue SQLite3::SQLException => e
+      puts e.backtrace
+      rs = nil
+    end
+    if rs != nil
+      temperatures = []
+      rs.each do |temperature_reading, source, timestamp|
+        temperatures << Temperature.new(temperature_reading, source, timestamp)
+      end
+      temperatures
+    end
+  end
+
 end
 
 class YrTemperature < Temperature
@@ -46,5 +109,9 @@ class YrTemperature < Temperature
     temperature_reading = document.xpath("//weatherdata/observations/weatherstation[@name='#{weatherstation}']/temperature/@value")
     timestamp = document.xpath("//weatherdata/observations/weatherstation[@name='#{weatherstation}']/temperature/@time")
     Temperature.new(temperature_reading.to_s, @source, Chronic.parse(timestamp.to_s).localtime.to_s)
+  end
+
+  def self.find_all
+    find_by_source(@source)
   end
 end
